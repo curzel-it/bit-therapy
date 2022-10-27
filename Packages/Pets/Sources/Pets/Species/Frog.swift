462e87f7512139a7f65485243046ebfbc067fca2
Federico Curzel <federico@curzel.it>
2022-10-27 10:35:18 +0200

    New assets for frogs

--- Packages/Pets/Sources/Pets/Species/Frog.swift
+++ Packages/Pets/Sources/Pets/Species/Frog.swift
@@ -10,17 +10,13 @@ extension Pet {
                 possibleAnimations: [
                     .front.with(loops: 5),
                     .side.with(loops: 5),
-                    .eatFly.with(loops: 1)
+                    .eat.with(loops: 3)
                 ]
             )
         ],
         movementPath: .walk,
-        speed: 1.2
+        speed: 1
     )
     
     static let frogVenom = Pet.frog.shiny(id: "frog_venom", isPaid: true)
-}
-
-private extension EntityAnimation {
-    static let eatFly = EntityAnimation(id: "eat", size: .init(width: 2, height: 1))
 }
