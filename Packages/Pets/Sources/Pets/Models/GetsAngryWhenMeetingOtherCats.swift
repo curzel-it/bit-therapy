213e8963064cb4f15fc4f3a8cf4b232465fad4ed
Federico Curzel <federico@curzel.it>
2023-01-10 20:48:07 +0100

    Adjusted timing of grumpy cat anger issues

--- Packages/Pets/Sources/Pets/Models/GetsAngryWhenMeetingOtherCats.swift
+++ Packages/Pets/Sources/Pets/Models/GetsAngryWhenMeetingOtherCats.swift
@@ -3,27 +3,30 @@
 //
 
 import Foundation
+import Schwifty
 import Yage
 
 class GetsAngryWhenMeetingOtherCats: Capability {
     override func update(with collisions: Collisions, after time: TimeInterval) {
-        guard let subject else { return }
-        guard subject.state != .freeFall && subject.state != .drag else { return }
+        guard isEnabled else { return }
+        guard subject?.state == .move else { return }
         guard isTouchingAnotherCat(accordingTo: collisions) else { return }
-        guard !isAngry() else { return }
-        subject.set(state: .action(action: .angry, loops: 5))
+        getAngry()
     }
-
-    private func isAngry() -> Bool {
-        if case .action(let anim, _) = subject?.state {
-            return anim.id == "angry"
+    
+    private func getAngry() {
+        guard let subject else { return }
+        Logger.log(subject.id, "Getting angry!")
+        subject.set(state: .action(action: .angry, loops: 4))
+        isEnabled = false
+        DispatchQueue.main.asyncAfter(deadline: .now() + 30) { [weak self] in
+            self?.isEnabled = true
         }
-        return false
     }
 
     private func isTouchingAnotherCat(accordingTo collisions: Collisions) -> Bool {
-        collisions.contains { collision in
-            collision.otherId.contains("cat")
+        collisions.contains {
+            $0.other?.species.id.hasPrefix("cat") == true
         }
     }
 }
