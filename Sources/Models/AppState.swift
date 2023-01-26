2094aeee0219c363bd8ba5978f0e2e7af7f8ed34
Federico Curzel <federico@curzel.it>
2023-01-26 18:05:37 +0100

    Decoupling AppState from Storage

--- Sources/Models/AppState.swift
+++ Sources/Models/AppState.swift
@@ -6,76 +6,28 @@ import Yage
 // MARK: - App State
 
 class AppState: ObservableObject {
-    static let global = AppState()
+    static let global = AppState(storage: AppStateStorageImpl())
 
     lazy var isDevApp: Bool = {
         let bundle = Bundle.main.bundleIdentifier ?? ""
         return bundle.contains(".dev")
     }()
     
-    @Published var desktopInteractions: Bool = true {
-        didSet {
-            storage.desktopInteractions = desktopInteractions
-        }
-    }
-
-    @Published var gravityEnabled: Bool = true {
-        didSet {
-            storage.gravityEnabled = gravityEnabled
-        }
-    }
-
-    @Published var petSize: CGFloat = 0 {
-        didSet {
-            storage.petSize = petSize
-        }
-    }
-
-    @Published private(set) var selectedSpecies: [Species] = [] {
-        didSet {
-            storage.selectedSpecies = selectedSpecies
-        }
-    }
-
-    @Published var showInMenuBar: Bool = true {
-        didSet {
-            storage.showInMenuBar = showInMenuBar
-        }
-    }
-
-    @Published var speedMultiplier: CGFloat = 1 {
-        didSet {
-            storage.speedMultiplier = speedMultiplier
-        }
-    }
-
-    @Published var trackingEnabled: Bool = false {
-        didSet {
-            storage.trackingEnabled = trackingEnabled
-        }
-    }
+    @Published var desktopInteractions: Bool = true
+    @Published var gravityEnabled: Bool = true
+    @Published var petSize: CGFloat = 0
+    @Published private(set) var selectedSpecies: [Species] = []
+    @Published var showInMenuBar: Bool = true
+    @Published var speedMultiplier: CGFloat = 1
+    @Published var trackingEnabled: Bool = false
+    @Published var disabledScreens: [String] = []
     
-    @Published var disabledScreens: [String] = [] {
-        didSet {
-            storage.disabledScreens = disabledScreens
-        }
-    }
+    private let storage: AppStateStorage
 
-    private let storage = Storage()
-
-    init() {
-        reload()
-    }
-
-    func reload() {
-        desktopInteractions = storage.desktopInteractions
-        gravityEnabled = storage.gravityEnabled
-        petSize = storage.petSize
-        selectedSpecies = storage.selectedSpecies
-        showInMenuBar = storage.showInMenuBar
-        speedMultiplier = storage.speedMultiplier
-        trackingEnabled = storage.trackingEnabled
-        disabledScreens = storage.disabledScreens
+    init(storage: AppStateStorage) {
+        self.storage = storage
+        readFromStorage()
+        storage.storeValues(of: self)
     }
     
     func isEnabled(screen: NSScreen) -> Bool {
@@ -98,9 +50,36 @@ class AppState: ObservableObject {
     func remove(species: Species) {
         selectedSpecies.remove(species)
     }
+
+    private func readFromStorage() {
+        desktopInteractions = storage.desktopInteractions
+        gravityEnabled = storage.gravityEnabled
+        petSize = storage.petSize
+        selectedSpecies = storage.selectedSpecies
+        showInMenuBar = storage.showInMenuBar
+        speedMultiplier = storage.speedMultiplier
+        trackingEnabled = storage.trackingEnabled
+        disabledScreens = storage.disabledScreens
+    }
 }
 
-private class Storage {
+// MARK: - Storage Interface
+
+protocol AppStateStorage {
+    var desktopInteractions: Bool { get }
+    var disabledScreens: [String] { get }
+    var gravityEnabled: Bool { get }
+    var petSize: Double { get }
+    var selectedSpecies: [Species] { get }
+    var showInMenuBar: Bool { get }
+    var speedMultiplier: Double { get }
+    var trackingEnabled: Bool { get }
+    func storeValues(of appState: AppState)
+}
+
+// MARK: - Storage Implementation
+
+private class AppStateStorageImpl: AppStateStorage {
     @AppStorage("desktopInteractions") var desktopInteractions: Bool = true
     @AppStorage("disabledScreens") var disabledScreensValue: String = ""
     @AppStorage("gravityEnabled") var gravityEnabled = true
@@ -133,6 +112,42 @@ private class Storage {
         set {
             selectedSpeciesValue = newValue.map { $0.id }.joined(separator: ",")
         }
+    }
+    
+    private var disposables = Set<AnyCancellable>()
+        
+    func storeValues(of appState: AppState) {
+        appState.$desktopInteractions
+            .sink { [weak self] in self?.desktopInteractions = $0 }
+            .store(in: &disposables)
+        
+        appState.$gravityEnabled
+            .sink { [weak self] in self?.gravityEnabled = $0 }
+            .store(in: &disposables)
+        
+        appState.$petSize
+            .sink { [weak self] in self?.petSize = $0 }
+            .store(in: &disposables)
+        
+        appState.$selectedSpecies
+            .sink { [weak self] in self?.selectedSpecies = $0 }
+            .store(in: &disposables)
+        
+        appState.$showInMenuBar
+            .sink { [weak self] in self?.showInMenuBar = $0 }
+            .store(in: &disposables)
+        
+        appState.$speedMultiplier
+            .sink { [weak self] in self?.speedMultiplier = $0 }
+            .store(in: &disposables)
+        
+        appState.$trackingEnabled
+            .sink { [weak self] in self?.trackingEnabled = $0 }
+            .store(in: &disposables)
+        
+        appState.$disabledScreens
+            .sink { [weak self] in self?.disabledScreens = $0 }
+            .store(in: &disposables)
     }
 }
 
