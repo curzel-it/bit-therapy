5ba7167dcf12567244f2b3a12db6271cb5a75323
Federico Curzel <federico@curzel.it>
2023-02-12 23:25:13 +0100

    New contributors, updated schwifty

--- Packages/CustomPets/Package.swift
+++ Packages/CustomPets/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../DependencyInjectionUtils"),
-        .package(url: "https://github.com/curzel-it/schwifty", from: "1.0.17"),
+        .package(url: "https://github.com/curzel-it/schwifty", from: "1.0.18"),
         .package(url: "https://github.com/Swinject/Swinject", from: "2.8.0"),
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.9")
     ],
