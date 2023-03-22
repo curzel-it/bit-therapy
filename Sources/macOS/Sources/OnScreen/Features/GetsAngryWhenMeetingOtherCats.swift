22ad3d4b132863a2fe43a79a698de1280562a0fb
Federico Curzel <federico@curzel.it>
2023-03-22 19:07:59 +0100

    Fixed angry animation for grumpy cat

--- Sources/macOS/Sources/OnScreen/Features/GetsAngryWhenMeetingOtherCats.swift
+++ Sources/macOS/Sources/OnScreen/Features/GetsAngryWhenMeetingOtherCats.swift
@@ -14,9 +14,21 @@ class GetsAngryWhenMeetingOtherCats: Capability {
         Logger.log(subject.id, "Getting angry!")
         subject.set(state: .action(action: .angry, loops: 4))
         isEnabled = false
+        DispatchQueue.main.asyncAfter(deadline: .now() + 10) { [weak self] in
+            self?.stopBeingAngry()
+        }
         DispatchQueue.main.asyncAfter(deadline: .now() + 30) { [weak self] in
             self?.isEnabled = true
         }
+    }
+    
+    private func stopBeingAngry() {
+        guard let subject = subject as? PetEntity else { return }
+        guard case .action(let action, _) = subject.state else { return }
+        guard action.id == "angry" else { return }
+        subject.direction = .init(dx: -1, dy: 0)
+        subject.resetSpeed()
+        subject.set(state: .move)
     }
 
     private func isTouchingAnotherCat(accordingTo collisions: Collisions) -> Bool {
