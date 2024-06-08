8bdfecf3ca987651a3800532f16c1732104ca8e9
Federico Curzel <federico@curzel.it>
2024-06-08 16:07:13 +0200

    Fixed ios build issue

---
+++ Sources/macOS/Sources/StatusBar/NoStatusBar.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+class StatusBarCoordinator {
+    static let shared = StatusBarCoordinator()
+
+    func show() {
+        // ...
+    }
+
+    func hide() {
+        // ...
+    }
+}
